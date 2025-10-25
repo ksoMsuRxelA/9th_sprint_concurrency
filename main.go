1a92b421f5387785bf81ae60ee26b2d9f7d44643
Ruslan Moskvitin <ra.moskvitin@yandex.ru>
2025-10-26 00:03:40 +0300

    feat: implement main logic and tests for first attempt

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand/v2" // v2 cuz the seed changes in every single "run"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +15,83 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		return nil
+	}
+
+	res := make([]int, 0, size)
+	var value, sign int
+
+	for i := 0; i < size; i++ {
+		value = rand.Int()
+		sign = rand.IntN(2)
+		if sign == 0 {
+			res = append(res, -value)
+		} else {
+			res = append(res, value)
+		}
+	}
+	return res
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if data == nil || len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	ans := math.MinInt
+	for _, value := range data {
+		if value > ans {
+			ans = value
+		}
+	}
+	return ans
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	var wg sync.WaitGroup
+
+	maxes := make([]int, CHUNKS)
+	chunkLength := len(data) / CHUNKS
+	var left, right int
+
+	wg.Add(CHUNKS)
+	for i := 0; i < CHUNKS; i++ { // i has iterable scope since Go 1.22 version (*)
+		left = chunkLength * i
+		right = left + chunkLength
+
+		go func(chunk []int) {
+			defer wg.Done()
+
+			maxes[i] = maximum(chunk) // due to (*) there will not be any race-condition & "i" comes from closure
+		}(data[left:right])
+	}
+	wg.Wait()
+
+	return maximum(maxes)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	var max int
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
-
+	start := time.Now()
+	max = maximum(data)
+	elapsed := time.Since(start).Microseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(data)
+	elapsed = time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
